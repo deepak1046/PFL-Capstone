e1b7f8ef64aaf3d18d3f93e84c06b8f046a434c0
Deepak SN <rockingdeepak1046@gmail.com>
2026-03-15 23:18:51 +0530

    Updates have been made 2

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,6 +1,8 @@
+const RENDER_URL = "https://noise-backend.onrender.com";
+const baseUrl = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") ? "http://localhost:3000" : RENDER_URL;
+
 async function loadData() {
 
-  const baseUrl = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? 'http://localhost:3000' : 'https://my-haskell-backend.onrender.com';
   const response = await fetch(`${baseUrl}/noise/average`);
 
   const data = await response.json();
